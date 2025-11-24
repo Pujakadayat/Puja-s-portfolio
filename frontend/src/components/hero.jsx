17ca898c5da34b463dc04cf4c48688033a1d7a53
pujakadayatdata <pujakadayat1@gmail.com>
2025-11-24 18:29:16 +0545

    added cv

--- frontend/src/components/hero.jsx
+++ frontend/src/components/hero.jsx
@@ -1,5 +1,6 @@
 import { useState,useEffect } from "react";
-import   HeroPic from "../sdcv.pdf";
+import   HeroPic from "../assets/pujaapp.png"
+import Puja from "../assets/puja.pdf";
 
 function Hero(){
     return (
